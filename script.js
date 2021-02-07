20a0d77a7b4b6691136dbad4f475db733da82f48
imborhade <sandeephborhade@gmail.com>
2021-02-07 20:46:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 function compute()
-      {
+{
 	var principal=document.getElementById('principal').value;
 	var interest=document.getElementById('rate').value;
 	var year=document.getElementById('years').value;
 	var d = new Date();
 	 var n = d.getFullYear()+parseInt(year);
-				
+	
+  if(principal<0)
+    {
+      alert("Please Enter Positive Amount.");
+      document.getElementById("principal").focus();
+      return;
+    }
+    if(principal==0)
+    {
+      alert("Please Enter  Amount.");
+      document.getElementById("principal").focus();
+      return;
+    }
+  
   var amount=parseInt(principal)*(parseInt(interest)*0.01)*parseInt(year);
         var result=`If you deposit <span class="h">${principal}</span>,<br/> 
       at an interest rate of <span class="h">${interest}</span> %.<br/>
