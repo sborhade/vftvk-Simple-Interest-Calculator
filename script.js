33b666a79065724d055e734ccd7fef20b3d46017
imborhade <sandeephborhade@gmail.com>
2021-02-07 18:57:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
-}
-        
+    function compute()
+      {
+				var principal=document.getElementById('principal').value;
+				var interest=document.getElementById('rate').value;
+				var year=document.getElementById('years').value;
+				
+        var amount=principal+(principal*interest*year);
+        var result=`If you deposit ${principal},<br/> 
+      at an interest rate of ${interest} %.<br/>
+      You will receive an amount of ${amount},<br/>
+      in the year ${year}`;
+        console.log(result);
+				document.getElementById('result').innerHTML =result;
+      }
