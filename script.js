20ae8678d05e9f7d3c06d10c67ba32be33270896
imborhade <sandeephborhade@gmail.com>
2021-02-07 19:14:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
     function compute()
       {
-				var principal=document.getElementById('principal').value;
-				var interest=document.getElementById('rate').value;
-				var year=document.getElementById('years').value;
+	var principal=document.getElementById('principal').value;
+	var interest=document.getElementById('rate').value;
+	var year=document.getElementById('years').value;
+	var d = new Date();
+	 var n = d.getFullYear()+parseInt(year);
 				
         var amount=principal+(principal*interest*year);
-        var result=`If you deposit ${principal},<br/> 
-      at an interest rate of ${interest} %.<br/>
-      You will receive an amount of ${amount},<br/>
-      in the year ${year}`;
+        var result=`If you deposit <span class="h">${principal}</span>,<br/> 
+      at an interest rate of <span class="h">${interest}</span> %.<br/>
+      You will receive an amount of<span class="h"> ${amount}</span>,<br/>
+      in the year <span class="h">${n}</span>`;
         console.log(result);
+        
 				document.getElementById('result').innerHTML =result;
       }
